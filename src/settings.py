c4d27e9e11244c2abf0aed477f8e0da23a403011
Milad Mahmoodi <info@miladmahmoodi.ir>
2023-08-14 00:27:29 +0330

    refactor: To modify the INSTALLED_APPS setting and add rest_framework to THIRD_PARTY_APPS

--- src/settings.py
+++ src/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+SYSTEM_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+DEVELOPED_APPS = [
+]
+
+THIRD_PARTY_APPS = [
+    'rest_framework',
+]
+
+INSTALLED_APPS = SYSTEM_APPS + DEVELOPED_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
