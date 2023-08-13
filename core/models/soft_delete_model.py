2e35d7497ee61c0c6d084cd30566101e07854cc3
Milad Mahmoodi <info@miladmahmoodi.ir>
2023-08-14 01:18:02 +0330

    build: A module called soft_delete_model has been created, along with the SoftDeleteModel class.

---
+++ core/models/soft_delete_model.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.utils.translation import gettext as _
+
+from core.models import BaseModel
+from core.custom_manager import CustomManager
+
+
+class SoftDeleteModel(BaseModel):
+    """
+    Abstract base model for soft-deleting records.
+    """
+    is_active = models.BooleanField(
+        verbose_name=_('is_active'),
+        default=True,
+    )
+
+    objects = CustomManager()
+
+    def delete(self, using=None, keep_parents=False) -> None:
+        """
+        Soft deletes the model instance.
+        """
+        self.is_active = False
+        self.save()
+
+    class Meta:
+        """
+        This class defines metadata options for the SoftDeleteModel model.
+        """
+        abstract = True
