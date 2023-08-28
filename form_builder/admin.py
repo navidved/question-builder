a21fc44c45cce79bfd27cce48e68c35486bc2fdf
Mahsa R <mahsarm99@gmail.com>
2023-08-28 21:24:24 +0330

    create VisitorAnswer admin

--- form_builder/admin.py
+++ form_builder/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from models import visitor_answer
 
 # Register your models here.
+@admin.register(visitor_answer)
+class VisitorAnswerAdmin(admin.ModelAdmin):
+    list_display = ['form','form_item','answer']
+    search_fields = ['form_item__text','visitor_id']
+    # list_filter = ['']
