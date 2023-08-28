70640d3f917e7b432a960c695af152485798e2cd
Mahsa R <mahsarm99@gmail.com>
2023-08-28 20:50:29 +0330

    add visitor_id field to VisitorAnswer model

--- form_builder/models/visitor_answer.py
+++ form_builder/models/visitor_answer.py
@@ -1,23 +1,27 @@
 from django.db import models
 from core.models import SoftDeleteModel, CreatedAtStampMixin, UpdatedAtStampMixin
 from django.utils.translation import gettext_lazy as _
+from uuid import uuid4
 
-
-class VisitorAnswer(CreatedAtStampMixin, UpdatedAtStampMixin, SoftDeleteModel):
+class VisitorAnswer(SoftDeleteModel):
+    visitor_id = models.UUIDField(
+        verbose_name=_('VisitorID'),
+        default=uuid4)
+    
     form = models.ForeignKey(
         'form_builder.Form', 
         on_delete=models.CASCADE, 
-        related_name='form_answers')
+        related_name='answers')
     
-    question = models.ForeignKey(
+    form_item = models.ForeignKey(
         'form_builder.FormItem', 
         on_delete=models.CASCADE, 
-        related_name='question_answers')
+        related_name='answers')
     
     answer = models.JSONField(verbose_name=_("Answer"))
     
     def __str__(self) -> str:
-        return f'{self.id}'
+        return self.id
     
 
     
