6b1c44737ee222c5c91e1ff816d3e45809dcee26
Mahsa R <mahsarm99@gmail.com>
2023-09-07 00:43:36 +0330

    create visitor model

---
+++ backend/form_builder/models/visitor.py
@@ -0,0 +1,19 @@
+from django.db import models
+from core.models import BaseModel,CreatedAtStampMixin
+
+
+class Visitor(CreatedAtStampMixin, BaseModel):
+    AUTHENTICATION_CHOICES = [
+    ("AN", "Ananymous"),
+    ("EM", "Email"),
+    ("PH", "Phone number")
+    ]
+    auth_type = models.CharField(
+        max_length = 2,
+        choices = AUTHENTICATION_CHOICES,
+        default = 'Anonymous')
+    
+    auth_value = models.CharField(
+        max_length=20)
+    
+    
