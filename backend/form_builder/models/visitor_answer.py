146003c2cd43a9f7f8fa68eb3c1bab1c46efa0a0
Mahsa R <mahsarm99@gmail.com>
2023-09-07 00:18:59 +0330

    change visitor_id field to foreignkey

--- backend/form_builder/models/visitor_answer.py
+++ backend/form_builder/models/visitor_answer.py
@@ -1,13 +1,13 @@
 from django.db import models
 from core.models import SoftDeleteModel
 from django.utils.translation import gettext_lazy as _
-from uuid import uuid4
 from django.db.models import Manager
 
 class VisitorAnswer(SoftDeleteModel):
-    visitor_id = models.UUIDField(
-        verbose_name=_('VisitorID'),
-        default=uuid4)
+    visitor_id = models.ForeignKey(
+        'form_builder.Visitor',
+        on_delete=models.CASCADE,
+        related_name='answers')
     
     form = models.ForeignKey(
         'form_builder.Form', 
