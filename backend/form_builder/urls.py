5180ff452cfc6f47c653a62c9c193415826c93b7
Mahsa R <mahsarm99@gmail.com>
2023-09-14 20:40:41 +0330

    correct visitors urls names

--- backend/form_builder/urls.py
+++ backend/form_builder/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
-
 from . import views
 
-app_name = 'form_builder'
+app_name = "form_builder"
 
 urlpatterns = [
-    path('create/', views.VisitorAuthenticationView.as_view(), name='create-visitor'),
-    path('answer/', views.AddVisitorAnswerView.as_view(), name='add-answer'),
-    path('answer/<int:answer_id>/', views.UpdateVisitorAnswerView.as_view(), name='update-answer'),
-    path('<slug:form_slug>/', views.GettingFormToAnswerView.as_view(), name='get-form'),
+    path("auth/", views.VisitorAuthenticationView.as_view(), name="create-visitor"),
+    path("answer/create/", views.AddVisitorAnswerView.as_view(), name="add-answer"),
+    path(
+        "answer/update/<int:answer_id>/",
+        views.UpdateVisitorAnswerView.as_view(),
+        name="update-answer",
+    ),
+    path(
+        "form/<slug:form_slug>/",
+        views.GettingFormToAnswerView.as_view(),
+        name="get-form",
+    ),
 ]
