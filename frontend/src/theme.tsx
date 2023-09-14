b7e0c3af7df360c87aeee6948b5b65edf375c060
Safoora Heidari <safoora.heidari@yahoo.com>
2023-09-14 08:50:17 +0330

    font changed

--- frontend/src/theme.tsx
+++ frontend/src/theme.tsx
@@ -1,6 +1,6 @@
 import { createTheme } from "@mui/material/styles";
 import { red } from "@mui/material/colors";
-import BNazanin from "./assets/fonts/BNazanin.woff";
+import Yekan from "./assets/fonts/Yekan.woff";
 
 // A custom theme for this app
 const theme = createTheme({
@@ -17,17 +17,17 @@ const theme = createTheme({
   },
   direction: "rtl",
   typography: {
-    fontFamily: ["BNazanin"].join(","),
+    fontFamily: ["Yekan"].join(","),
   },
   components: {
     MuiCssBaseline: {
       styleOverrides: `
         @font-face {
-          font-family: 'BNazanin';
+          font-family: 'Yekan';
           font-style: normal;
           font-display: swap;
           font-weight: 400;
-          src: local('BNazanin'), local('BNazanin-Regular'), url(${BNazanin}) format('woff');
+          src: local('Yekan'), local('Yekan-Regular'), url(${Yekan}) format('woff');
           unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
         }
       `,
@@ -35,13 +35,13 @@ const theme = createTheme({
     MuiTypography: {
       defaultProps: {
         variantMapping: {
-          h1: "h2",
+          h1: "h1",
           h2: "h2",
-          h3: "h2",
-          h4: "h2",
-          h5: "h2",
-          h6: "h2",
-          subtitle1: "h2",
+          h3: "h3",
+          h4: "h4",
+          h5: "h5",
+          h6: "h6",
+          subtitle1: "h1",
           subtitle2: "h2",
           body1: "span",
           body2: "span",
