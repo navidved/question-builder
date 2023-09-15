b033beaf970c06769995a8bfa02017fcf133e0fe
Safoora Heidari <safoora.heidari@yahoo.com>
2023-09-15 23:28:45 +0330

    create visitor api changed

--- frontend/src/components/custom-hooks/use-create-visitor.tsx
+++ frontend/src/components/custom-hooks/use-create-visitor.tsx
@@ -1,19 +1,15 @@
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 
-interface Visitor {
-  form: number;
-  auth_type: string;
-  auth_value: string;
-}
+// interface Visitor {
+//   form: number;
+//   auth_type: string;
+//   auth_value: string;
+// }
 
-const createVisitor = async (data: Visitor) => {
-  const { data: response } = await axios.post(
-    "http://127.0.0.1:8000/api/visitor/create",
-    data
-  );
-  return response.data;
-};
+function createVisitor(data) {
+  return axios.post("http://127.0.0.1:8000/api/visitor/auth/", data);
+}
 
 export default function useCreateVisitor() {
   return useMutation(createVisitor);
