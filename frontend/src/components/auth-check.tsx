ed2e035e8aa451204d435837e34ed9ae6e2257d2
Safoora Heidari <safoora.heidari@yahoo.com>
2023-09-15 23:29:12 +0330

    authentication page

--- frontend/src/components/auth-check.tsx
+++ frontend/src/components/auth-check.tsx
@@ -2,15 +2,9 @@ import { Stack, TextField, Typography } from "@mui/material";
 
 export default function AuthCheck({
   auth_method,
-  formItem,
-  setFormItem,
-  formAuthValue,
   setFormAuthValue,
 }: {
   auth_method: string;
-  formItem: number;
-  setFormItem: any;
-  formAuthValue: string;
   setFormAuthValue: any;
 }) {
   return (
@@ -18,12 +12,17 @@ export default function AuthCheck({
       {auth_method == "PH" ? (
         <Stack>
           <Typography variant="h5" mb={3}>
-            َشماره تلفن خود را وارد کنید
+            شماره تلفن خود را وارد کنید
           </Typography>
           <TextField onChange={(e) => setFormAuthValue(e.target.value)} />
         </Stack>
       ) : auth_method == "EM" ? (
-        <Typography>ایمیل خود را وارد کنید</Typography>
+        <Stack>
+          <Typography variant="h5" mb={3}>
+            ایمیل خود را وارد کنید
+          </Typography>
+          <TextField onChange={(e) => setFormAuthValue(e.target.value)} />
+        </Stack>
       ) : (
         <Typography>خوش آمدید</Typography>
       )}
