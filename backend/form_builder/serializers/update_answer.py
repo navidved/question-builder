a2ac9ddf600a199c95670d2e436eeeb898d3d917
Mahsa R <mahsarm99@gmail.com>
2023-09-16 23:19:06 +0330

    edit updateanswer serializer

--- backend/form_builder/serializers/update_answer.py
+++ backend/form_builder/serializers/update_answer.py
@@ -1,10 +1,20 @@
 from rest_framework import serializers
 from form_builder.models import VisitorAnswer
-
+from form_builder.validators import answer_validator
 
 class UpdateAnswerSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-
     class Meta:
         model = VisitorAnswer
-        fields = ["id", "visitor", "form", "form_item", "answer"]
+        fields = ["id", "answer"]
+
+    def validate_answer(self, value: dict):
+        res = answer_validator(self.context.get("answer_type"), value)
+        if res:
+            return value
+        raise serializers.ValidationError("Incorrect Answer")
+
+    def update(self, instance: VisitorAnswer, validated_data: dict):
+        # validated_data.pop('answer_type')
+        instance.answer = validated_data.get("answer")
+        instance.save()
+        return instance
