0dd1c526becda0c0adffc27cf50843e9c9e2a80a
Mahsa R <mahsarm99@gmail.com>
2023-09-16 22:40:26 +0330

    Revert "edit updateanswer serializer"
    This reverts commit dd31f4194467f2ab05f973d002018875240e5814.

--- backend/form_builder/validators/answer.py
+++
@@ -1,4 +0,0 @@
-def answer_validator(answer_type, answer):
-    if answer_type in answer and len(answer) == 1:
-        return True
-    return False
--- backend/form_builder/views/update_answer.py
+++ backend/form_builder/views/update_answer.py
@@ -12,15 +12,11 @@ from form_builder.serializers import UpdateAnswerSerializer
 class UpdateAnswerView(APIView):
     def patch(self, request: Request, answer_id):
         answer = get_object_or_404(VisitorAnswer, id=answer_id)
-        context = request.data["answer_type"]
         answer_srz = UpdateAnswerSerializer(
-            instance=answer,
-            data=request.data,
-            partial=True,
-            context={"answer_type": context},
+            instance=answer, data=request.data, partial=True
         )
         if answer_srz.is_valid():
-            answer_srz.update(answer, answer_srz.validated_data)
-            data = answer_srz.data
+            answer_srz.save()
+            data = {"message": "Answer Updated successfully.", "data": answer_srz.data}
             return Response(data, status=status.HTTP_200_OK)
         return Response(answer_srz.errors, status=status.HTTP_400_BAD_REQUEST)
