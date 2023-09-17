f7e0ae86209dbeb53b52f02aba8d7c748fd9e50d
Ramin <eslamiramin85@gmail.com>
2023-09-16 17:21:16 -0700

    create CreateVisitorView

--- backend/form_builder/serializers/create_visitor.py
+++ backend/form_builder/serializers/create_visitor.py
@@ -1,8 +1,7 @@
 import re
-
+import uuid
 from rest_framework import serializers
-from django.core.validators import
-from form_builder.models import Visitor, VisitorAnswer
+from form_builder.models import Visitor, VisitorAnswer, Form
 from form_builder.serializers import UpdateAnswerSerializer
 
 
@@ -11,39 +10,62 @@ def validate_email(email):
         return False
     return True
 
+
 def validate_phone_number(phone_number):
     if not re.match(r"^(?:\+98|0)?9\d{9}$", phone_number):
         return False
     return True
 
-class VisitorAnswersSerializer2(serializers.ModelSerializer):
-    form_item = serializers.IntegerField(label='form_item_id')
-    answer_type = serializers.SerializerMethodField()
 
+class VisitorAnswersSerializer2(serializers.ModelSerializer):
+    form_item_id = serializers.SerializerMethodField(read_only=True)
+    answer_type = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = VisitorAnswer
         fields = [
-            'form_item',
+            'form_item_id',
             'answer_type',
             'answer',
         ]
 
     def get_answer_type(self, obj):
-        return obj.answers.answer_type
+        return obj.form_item.answer_type
+
+    def get_form_item_id(self, obj):
+        return obj.form_item.id
+
 
 class VisitorAuthSerializer(serializers.Serializer):
     form_id = serializers.IntegerField()
     auth_type = serializers.CharField(write_only=True)
-    auth_value = serializers.CharField(write_only=True)
-    visitor_id = serializers.IntegerField(read_only=True)
+    auth_value = serializers.CharField(write_only=True, allow_blank=True)
+    visitor_id = serializers.SerializerMethodField(read_only=True)
     visitor_answers = serializers.SerializerMethodField(read_only=True)
 
+    def get_visitor_id(self, *args):
+        return self.context.get('visitor_id')
+
     def get_visitor_answers(self, obj):
-        ...
+        form = Form.objects.get(id=self.context.get('form_id'))
+        visitor = Visitor.objects.get(id=self.context.get('visitor_id'))
+
+        answers = VisitorAnswer.objects.filter(
+            form = form, visitor = visitor
+        )
+        print(answers)
+        return VisitorAnswersSerializer2(instance=answers, many=True).data
 
     def create(self, validated_data):
-        ...
+        if validated_data['auth_value'] == "":
+            validated_data["auth_value"] = uuid.uuid4().hex
+        visitor = Visitor.objects.create()
+        visitor.auth_type = validated_data['auth_type']
+        visitor.auth_value = validated_data['auth_value']
+        visitor.save()
+        form = Form.objects.get(id=validated_data['form_id'])
+        visitor.form.add(form)
+        return visitor
 
     def validate_auth_type(self, value):
         valid_auth_type = ['AN', 'EM', 'PH']
@@ -63,17 +85,9 @@ class VisitorAuthSerializer(serializers.Serializer):
                 raise serializers.ValidationError('The phone number is invalid')
             return data
 
-        else:
-            if not data['auth_value'] == '':
-                raise serializers.ValidationError('the auth value is invalid')
-            return data
-
-
-
-
-
-
-
+        if data['auth_value'] != '':
+            raise serializers.ValidationError('AN ERROR')
+        return data
 
 
 class VisitorSerializer(serializers.ModelSerializer):
--- backend/form_builder/views/create_visitor.py
+++ backend/form_builder/views/create_visitor.py
@@ -5,10 +5,10 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from form_builder.models import Form, Visitor, VisitorForm
-from form_builder.serializers import VisitorSerializer, VisitorFormSerializer
+from form_builder.serializers import VisitorSerializer, VisitorFormSerializer , VisitorAuthSerializer
 
 
-class CreateVisitorView(APIView):
+class CreateVisitorView2(APIView):
     def post(self, request):
         try:
             form_id = request.data["form"]
@@ -47,3 +47,33 @@ class CreateVisitorView(APIView):
             return Response(
                 {"Error": "Invalid data provided"}, status=status.HTTP_400_BAD_REQUEST
             )
+
+
+class CreateVisitorView(APIView):
+    def post(self, request):
+        srz_data = VisitorAuthSerializer(data=request.data, context={"form_id": request.data['form_id']})
+        if srz_data.is_valid():
+            try:
+                visitor = Visitor.objects.get(auth_value=request.data['auth_value'])
+            except Visitor.DoesNotExist:
+                visitor = None
+            if visitor:
+                visitor_srz = VisitorAuthSerializer(
+                    instance=request.data,
+                    context={
+                        "form_id": request.data['form_id'],
+                        "visitor_id": visitor.id,
+                    }
+                )
+                return Response(data=visitor_srz.data)
+            if visitor is None:
+                visitor = srz_data.create(validated_data=srz_data.validated_data)
+                visitor_srz = VisitorAuthSerializer(
+                    instance=request.data,
+                    context={
+                        "form_id": request.data['form_id'],
+                        "visitor_id": visitor.id,
+                    }
+                )
+                return Response(data=visitor_srz.data)
+        return Response(data=srz_data.errors)
