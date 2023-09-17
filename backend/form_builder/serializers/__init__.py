366a54a9cb64a960f6e2c86170b59925bc7db573
Ramin <eslamiramin85@gmail.com>
2023-09-16 17:34:08 -0700

    modify __init__

--- backend/form_builder/serializers/__init__.py
+++ backend/form_builder/serializers/__init__.py
@@ -1,5 +1,5 @@
 from .form_item import FormItemSerializer
 from .get_form import GetFormSerializer
 from .update_answer import UpdateAnswerSerializer
-from .create_visitor import VisitorSerializer, VisitorAuthSerializer
+from .create_visitor import VisitorSerializer, CreateVisitorSerializer
 from .visitor_form import VisitorFormSerializer
--- backend/form_builder/views/create_visitor.py
+++ backend/form_builder/views/create_visitor.py
@@ -3,19 +3,19 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from form_builder.models import Form, Visitor, VisitorForm
-from form_builder.serializers import VisitorSerializer, VisitorFormSerializer , VisitorAuthSerializer
+from form_builder.serializers import VisitorSerializer, VisitorFormSerializer , CreateVisitorSerializer
 
 
 class CreateVisitorView(APIView):
     def post(self, request):
-        srz_data = VisitorAuthSerializer(data=request.data, context={"form_id": request.data['form_id']})
+        srz_data = CreateVisitorSerializer(data=request.data, context={"form_id": request.data['form_id']})
         if srz_data.is_valid():
             try:
                 visitor = Visitor.objects.get(auth_value=request.data['auth_value'])
             except Visitor.DoesNotExist:
                 visitor = None
             if visitor:
-                visitor_srz = VisitorAuthSerializer(
+                visitor_srz = CreateVisitorSerializer(
                     instance=request.data,
                     context={
                         "form_id": request.data['form_id'],
@@ -25,7 +25,7 @@ class CreateVisitorView(APIView):
                 return Response(data=visitor_srz.data)
             if visitor is None:
                 visitor = srz_data.create(validated_data=srz_data.validated_data)
-                visitor_srz = VisitorAuthSerializer(
+                visitor_srz = CreateVisitorSerializer(
                     instance=request.data,
                     context={
                         "form_id": request.data['form_id'],
